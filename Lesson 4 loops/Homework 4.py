0f4232de82ad161d031850532512c24aad1fa5ec
Altynai <mambetova.altynai@gmail.com>
2022-03-04 18:02:01 +0600

    while if

---
+++ Lesson 4 loops/Homework 4.py
@@ -0,0 +1,22 @@
+name = input('Enter your name: ')
+
+print(f'{name * 20}')
+
+counter = 1
+
+while counter <= 25:
+    print(counter*5)
+    counter += 3
+
+counter2 = int(input('Enter a number: '))
+
+while counter2 > 0:
+    print('The result number +10: ', counter2 + 10)
+    counter2 = int(input('Enter a number: '))
+    if counter2 == 0:
+        print('Not defined')
+
+word = input('Enter a word:')
+
+for i in range(len(word)):
+    print(word[i]*2)
---
+++ Lesson 4 loops/Practice 4.py
@@ -0,0 +1,24 @@
+counter = -10
+
+while counter < 0:
+    print(counter)
+    counter += 1
+
+for counter in range (-10,0):
+    print(counter)
+
+counter2 = 13
+
+while counter2 >= 1:
+    print(counter2, end=" ")
+    counter2 -= 1
+
+counter3 = 34
+
+while counter3 < 67:
+    print(counter3)
+    counter3 += 2
+
+for i in range(34, 67+1, 2):
+    print(i, end=" ")
+    
