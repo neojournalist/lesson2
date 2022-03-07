817acb60599481f2c8f7b40e0666b8578163595e
Altynai <mambetova.altynai@gmail.com>
2022-03-07 16:34:23 +0600

    tuples

---
+++ Lesson 4 loops/tuples hm.py
@@ -0,0 +1,14 @@
+tuple1 = (11, 22, 33, 44, 55, 66)
+tuple2 = tuple1[3:5]
+print(tuple2)
+
+tuple3 = ('Anna', 'Ben', 'Carla', 'Cindy')
+
+for i in tuple3:
+    print('Hello ', i)
+
+aTuple = (10, 20, 30, 40, 50)
+a, b, c, d, e = aTuple
+print(aTuple[::-1])
+print(b)
+
