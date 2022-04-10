364c8a7891b4e0f51081a418e6391a66839d44b3
Altynai <mambetova.altynai@gmail.com>
2022-04-10 22:40:13 +0600

    read data

--- FileOperations/practice2.py
+++ FileOperations/practice2.py
@@ -26,3 +26,31 @@ for row in cells3:
         listCells.append(data.value)
 
 print(f'Median {stats.median(listCells)}')
+print(f'Average {stats.mean(listCells)}')
+print(f'Variance {stats.variance(listCells)}')
+print(f'St Deviation {stats.stdev(listCells)}')
+
+wb2 = Workbook()
+sheet2 = wb2.worksheets[0]
+
+regionNames = ['JB', 'Naryn', 'IK', 'Osh', 'Talas']
+cityNames = ['ABS', 'Kls', 'Pmn', 'YKL', 'Jnm']
+population = [100, 200, 300, 500, 700]
+mayorName = ['OL', 'JV', 'GB', 'HY', 'TY']
+
+columnName1 = sheet2.cell(row=1, column=1).value = 'Region'
+columnName2 = sheet2.cell(row=1, column=2).value = 'City'
+columnName3 = sheet2.cell(row=1, column=3).value = 'Population'
+columnName4 = sheet2.cell(row=1, column=4).value = 'Mayor'
+
+counter = 2
+for i in range(6):
+    sheet2.cell(row = counter, column = 1).value = regionNames[i]
+    sheet2.cell(row = counter, column = 2).value = cityNames[i]
+    sheet2.cell(row = counter, column = 3).value = population[i]
+    sheet2.cell(row = counter, column = 4).value = mayorName[i]
+    counter += 1
+
+wb2.save('KGInfo.xlsx')
+print('Successfully saved')
+
