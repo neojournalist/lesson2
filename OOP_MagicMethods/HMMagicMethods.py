916f907a11e4c577b745adbb0855b2001d55ee0e
Altynai <mambetova.altynai@gmail.com>
2022-05-04 23:41:55 +0600

    abstraction

---
+++ OOP_MagicMethods/HMMagicMethods.py
@@ -0,0 +1,40 @@
+class Area:
+    def __init__(self, h, w):
+        self.height = h
+        self.width = w
+        self.square = None
+
+    def findArea(self):
+        self.square = self.height * self.width
+        print(self.square)
+
+    def __lt__(self, other):
+        if self.square < other.sqaure:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.square > other.sqaure:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        if self.square == other.sqaure:
+            print()
+        else:
+            return False
+
+def main():
+    f1 = Area(5, 4)
+    f2 = Area(3, 5)
+    print(f1.findArea())
+    print(f2.findArea())
+
+
+    print(f1 > 10)
+
+
+if __name__ == '__main__':
+    main()
